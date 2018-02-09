eee71a3c7e2a00f75d7c085e3b17c7208235c352
William Lewis <william.lewis@wework.com>
2018-02-09 12:24:16 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,7 @@
 def position_taken?(board, index)
   if (board[index] == ' ') || (board[index] == '') || (board[index] == nil)
     false
+  else (board[index] == 'X') || (board[index] == 'O')
+    true
   end
 end
